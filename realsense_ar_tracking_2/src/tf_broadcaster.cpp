8a92004cff54dc29edc0378bcde5f22edab22d63
LThurston98102534 <48243639+LThurston98102534@users.noreply.github.com>
2020-11-17 15:47:30 +1100

    Updated broadcaster.cpp
    Updated broadcaster.cpp to contain both the calibrated and calculated (from CAD) values of the Base to Camera transform to allow for running of the tracker.cpp nodes for future testing.

--- realsense_ar_tracking_2/src/tf_broadcaster.cpp
+++ realsense_ar_tracking_2/src/tf_broadcaster.cpp
@@ -13,12 +13,20 @@ int main(int argc, char** argv){
 
   // Create a quaternion with calibrated base to camera results (SHOULD BE UPDATED WITH ANY RE-CALIBRATED RESULTS)
   tf::Quaternion base_camera_quaternion(-0.012, 0.007, 0.713, 0.701);
+  
+  // Base to Camera rotation transform from CAD file. This is the target the calibration process should aim to return as close to as possible. Can uncomment if desired for further testing
+  // tf::Quaternion base_camera_quaternion(0, 0., 0.7071068, 0.7071068);
+  
+  
   base_camera_quaternion = base_camera_quaternion.normalize();
 
   while(n.ok()){
 
      // Broadcast relative base to camera transform calculated during the calibration process (VALUES SHOULD BE UPDATED WITH ANY RE-CALIBRATED RESULTS)
      broadcaster.sendTransform(tf::StampedTransform(tf::Transform(base_camera_quaternion, tf::Vector3(-0.145, 0.04725, 0.21525)),ros::Time::now(),"base_link", "camera_base_link"));
+          
+     // Base to Camera Transform from CAD file. This is target the calibration process should aim to return as close to as possible. Can uncomment if desired for further testing
+     // broadcaster.sendTransform(tf::StampedTransform(tf::Transform(base_camera_quaternion, tf::Vector3(-0.152, 0.04857, 0.2075)),ros::Time::now(),"base_link", "camera_base_link"));
           
     
      // TO BE ADDED: RELATIVE TRANSFORMS BETWEEN SEALER/FRIDGE TAGS AND TARGET LOCATIONS
